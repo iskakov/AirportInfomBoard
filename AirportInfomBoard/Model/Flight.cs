a813d01f9438d836322d49cc796d37b6e9e3538c
ma3ca3@gmail.com <2121123Qq>
2020-06-27 20:25:22 +0500

    Добавление модели

---
+++ AirportInfomBoard/Model/Flight.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportInfomBoard.Model
+{
+    public class Flight
+    {
+        public int CountPassenger { get; set; }
+        private bool isFlight;
+        public bool IsFlight { 
+            get
+            {
+                return isFlight;
+            }
+            set 
+            {
+                isFlight = value;
+                if (value)
+                    IsFlightSt = "Вылетел";
+                else
+                    IsFlightSt = "Прилетел";
+            } 
+        }
+        public DateTime DateFlight { get; set; }
+        public String CityName { get; set; }
+        public String IsFlightSt { get; set; }
+
+        public Flight()
+        {
+            DateFlight = new DateTime();
+            CityName = "";
+        }
+
+        public Flight(int countPassenger, bool isFlight, DateTime dateFlight, string cityName)
+        {
+            CountPassenger = countPassenger;
+            IsFlight = isFlight;
+            DateFlight = dateFlight;
+            CityName = cityName;
+        }
+
+
+    }
+}
---
+++ AirportInfomBoard/Model/InformForGrapfic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportInfomBoard.Model
+{
+    public class InformForGrapfic
+    {
+        public int CountPassengerArrive { get; set; }
+        public int CountPassengerDeparture { get; set; }
+        public TimeSpan Time { get; set; }
+    }
+}
